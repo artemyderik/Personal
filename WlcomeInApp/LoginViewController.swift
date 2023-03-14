b8d10544ff9ac60f95fd5606cd94e470fd8d8087
ArtemyDerik <derry0808@gmail.com>
2023-03-14 09:52:02 +0700

    CleanAlertMethod

--- WlcomeInApp/LoginViewController.swift
+++ WlcomeInApp/LoginViewController.swift
@@ -11,44 +11,33 @@ class LoginViewController: UIViewController {
 
     @IBOutlet var usernameTextField: UITextField!
     @IBOutlet var passwordTextField: UITextField!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
 
     @IBAction func forgotPasswordButtonTapped() {
-        showForgotPasswordAlert(with: "Oops!", and: "Your password is Password")
+        showAlert(with: "Oops!", and: "Your password is Password")
     }
     @IBAction func forgotUserNameButtonTapped() {
-        showForgotUserNameAlert(with: "Oops!", and: "Your username is User")
+        showAlert(with: "Oops!", and: "Your username is User")
     }
 }
 
 
-//MARK: Username & Password Alerts
+//MARK: Alert method
 
 extension LoginViewController {
-     private func showForgotUserNameAlert(
+    
+     private func showAlert(
         with title: String?,
         and message: String?
     ) {
-        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let alert = UIAlertController(
+            title: title,
+            message: message,
+            preferredStyle: .alert
+        )
         let okAlert = UIAlertAction(title: "OK", style: .default)
         
         present(alert, animated: true)
         alert.addAction(okAlert)
     }
     
-     private func showForgotPasswordAlert(
-        with title: String?,
-        and message: String?
-    ) {
-        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        let okAlert = UIAlertAction(title: "OK", style: .default)
-        
-        present(alert, animated: true)
-        alert.addAction(okAlert)
-
-    }
 }
