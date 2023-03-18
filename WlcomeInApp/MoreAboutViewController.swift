7a75108bf2888f415cb5bcf0bbf1363b20d845f6
ArtemyDerik <derry0808@gmail.com>
2023-03-18 16:06:10 +0700

    MoreAboutViewControllerWasAdded

---
+++ WlcomeInApp/MoreAboutViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MoreAboutViewController.swift
+//  WlcomeInApp
+//
+//  Created by Артемий Дериглазов on 18.03.2023.
+//
+
+import UIKit
+
+class MoreAboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
