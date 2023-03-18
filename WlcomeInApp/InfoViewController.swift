f04640ca75c61dff8cb3612fbae6f80b99c980a8
ArtemyDerik <derry0808@gmail.com>
2023-03-18 16:05:20 +0700

    InfoViewControllerWasAdded

---
+++ WlcomeInApp/InfoViewController.swift
@@ -0,0 +1,18 @@
+//
+//  InfoController.swift
+//  WlcomeInApp
+//
+//  Created by Артемий Дериглазов on 18.03.2023.
+//
+
+import UIKit
+
+class InfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
