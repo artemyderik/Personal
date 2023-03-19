60f42bf68cee08566b78f5e9014ff7acdbec406d
ArtemyDerik <derry0808@gmail.com>
2023-03-19 17:01:54 +0700

    SetsettingsScreen

--- WlcomeInApp/ViewControllers/SettingsViewController.swift
+++ WlcomeInApp/ViewControllers/SettingsViewController.swift
@@ -9,13 +9,59 @@ import UIKit
 
 class SettingsViewController: UIViewController {
 
+    @IBOutlet var colorisedView: UIView!
+    
+    @IBOutlet var greenSliderValue: UILabel!
+    @IBOutlet var redSliderValue: UILabel!
+    @IBOutlet var blueSliderValue: UILabel!
+
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        colorisedView.layer.cornerRadius = colorisedView.frame.width / 15.5
+        slidersSetup()
+        toColorTheView()
     }
+    
+    @IBAction func redSpectorChanges() {
+        redSliderValue.text = String(format: "%.2f", redSlider.value)
+        toColorTheView()
+    }
+    
+    @IBAction func greenSpectorChanges() {
+        greenSliderValue.text = String(format: "%.2f", greenSlider.value)
+        toColorTheView()
+    }
+    
+    @IBAction func blueSpectorChanges() {
+        blueSliderValue.text = String(format: "%.2f", blueSlider.value)
+        toColorTheView()
+    }
+    
+    
      func unwind(for segue: UIStoryboardSegue, sender: Any?) {
        guard let tabBarController = segue.source as? TabBarController else { return }
     }
 
+    
+    // MARK: - Private methods
+    private func slidersSetup() {
+        redSlider.minimumTrackTintColor = UIColor.red
+        greenSlider.minimumTrackTintColor = UIColor.green
+        blueSlider.minimumTrackTintColor = UIColor.blue
+    }
+    
+    private func toColorTheView() {
+        colorisedView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
+    }
+
 }
+
