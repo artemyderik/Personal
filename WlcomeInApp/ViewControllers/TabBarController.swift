07886f9780384e4f90f1a54e956a507742797765
ArtemyDerik <derry0808@gmail.com>
2023-03-19 16:10:25 +0700

    classTabBarController

---
+++ WlcomeInApp/ViewControllers/TabBarController.swift
@@ -0,0 +1,16 @@
+//
+//  TabBarController.swift
+//  WlcomeInApp
+//
+//  Created by Артемий Дериглазов on 19.03.2023.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tabBar.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1)
+    }
+}
