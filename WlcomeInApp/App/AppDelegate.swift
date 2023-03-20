2980e2c6a4ffc14af6bb14368f95be4933e9149d
ArtemyDerik <derry0808@gmail.com>
2023-03-20 08:00:58 +0700

    MarkDel

--- WlcomeInApp/App/AppDelegate.swift
+++ WlcomeInApp/App/AppDelegate.swift
@@ -10,11 +10,6 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    
-    //СДЕЛАТЬ КНОПКУ ОБНУЛЕНИЯ В ЧЕРНЫЙ И В БЕЛЫЙ (УСТАНОВИТЬ ЗНАЧЕНИЯ НА 0 И 1) и добавиьть алерт
-    
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         UITabBar.appearance().tintColor = UIColor(red: 0.18, green: 0.5, blue: 1, alpha: 1)
         UITabBar.appearance().unselectedItemTintColor = UIColor(red: 0.18, green: 0.5, blue: 1, alpha: 0.7)
