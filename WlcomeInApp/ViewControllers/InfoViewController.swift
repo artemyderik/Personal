9252163e99faf626895c10b53e9ac2d514681f02
ArtemyDerik <derry0808@gmail.com>
2023-03-21 07:51:59 +0700

    userProperty&prepareForMethodAtInfoVC

--- WlcomeInApp/ViewControllers/InfoViewController.swift
+++ WlcomeInApp/ViewControllers/InfoViewController.swift
@@ -9,10 +9,16 @@ import UIKit
 
 class InfoViewController: UIViewController {
 
+    var user: User!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        title = "\(user.person.name) \(user.person.surname)"
     }
-
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let moreAboutVC = segue.destination as? MoreAboutViewController else { return }
+        moreAboutVC.user = user
+    }
 
 }
